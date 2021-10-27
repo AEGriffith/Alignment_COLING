3824c6267f0357dd1ffe8ce19c2940e465b3e775
Sylvia Tian <sylviatian935@gmail.com>
2021-10-27 00:29:48 -0400

    add fitted value and model prediction part

--- brms_model_1020.R
+++ brms_model_1020.R
@@ -21,7 +21,50 @@ model1_1 <- brm(count_target | trunc(lb=1) ~ count_prime_own + count_prime_partn
                 (1 | group)+ (1|group_speaker)+ (1|word) + (1|group_turn_id) + (1|term),
               data = word_list_data_study, family = poisson(),
                 chains = 4, cores = 4, backend = 'cmdstanr', threads = threading(8))
+
+# checking convergence and fit
 summary(model1_1)
+# newly added code
+plot(model1_1)
+pp = brms::pp_check(model1_1)
+pp + theme_bw()
+## Plotting model predictions
+brms::marginal_effects(model1_1)
+# get fitted values
+
+# option 1: 
+# newdata = data.frame(corpus = levels(word_list_data_study$corpus)) # Xiaoyi: not sure how to modify this for our data, maybe use 
+# fit = fitted(model1_1, newdata = newdata, re_formula = NA) * 100  # convert to %
+
+# option 2: 
+## compute expected predictions
+newdata = data.frame(word_list_data_study))
+fitted_values <- fitted(model1_1)
+head(fitted_values)
+## plot expected predictions against actual response
+dat <- as.data.frame(cbind(Y = standata(model1_1)$Y, fitted_values))
+ggplot(dat) + geom_point(aes(x = Estimate, y = Y))
+
+
+colnames(fit) = c('fit', 'se', 'lwr', 'upr') # this may not work? 
+df_plot = cbind(newdata, fitted_values)
+df_plot
+
+ggplot(df_plot, aes(x = corpus, y = fit)) +
+  geom_violin(data=obs, aes(x=corpus, y=real), alpha=0.5, color="gray70", fill='gray95') +
+  geom_jitter(data=obs, aes(x=corpus, y=real), alpha=0.3, position = position_jitter(width = 0.07)) +
+  geom_errorbar(aes(ymin=lwr, ymax=upr), position=position_dodge(), size=1, width=.5) +
+  geom_point(shape=21, size=4, fill='red') +
+  geom_hline(yintercept=50, linetype=2) +
+  xlab("") +
+  ylab('Probability of responding REAL, %') +
+  ylim(0,100) +
+  theme_bw () +
+  theme(panel.grid = element_blank())
+
+
+
+
 # 1.2 word aggregated across sentences for each speaker
 
 # aggregate across sentences
@@ -37,7 +80,10 @@ model1_2 <- brm(count_target | trunc(lb=1) ~ count_prime_own + count_prime_partn
                   (1 | term_group)+ (1|group_speaker)+ (1|word) + (1|term),
                 data = all_word_sum, family = poisson(),
 chains = 4, cores = 4, backend = 'cmdstanr', threads = threading(8))
+
+# checking convergence and fit
 summary(model1_2)
+
 
 
 # 2. task-relavant word models
